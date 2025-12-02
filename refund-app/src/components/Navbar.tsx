b8642aff95f60fef473d9d4926c0801adaa7c5c2
shuvams100-code <shuvams100@gmail.com>
2025-12-03 01:44:15 +0530

    Auto-sync: 03-12-2025  1:44:15.26

--- refund-app/src/components/Navbar.tsx
+++ refund-app/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
     return (
         <nav className="w-full bg-white border-b border-gray-100 h-16 flex items-center px-6 sticky top-0 z-50">
             <Link href="/" className="flex items-center">
-                <div className="relative h-8 w-24">
+                <div className="relative h-10 w-32">
                     <Image
-                        src="/assets/ryyt-icon-blue.png"
+                        src="/assets/ryyt-logo-blue.png"
                         alt="RYYT Logo"
                         fill
                         className="object-contain object-left"
