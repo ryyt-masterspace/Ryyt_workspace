65e4f1d1a62218c634e6d7f2cb6d6880b8e3803f
shuvams100-code <shuvams100@gmail.com>
2025-12-04 00:56:50 +0530

    Auto-sync: 04-12-2025  0:56:49.69

--- refund-app/src/app/layout.tsx
+++ refund-app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-import Navbar from "@/components/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
