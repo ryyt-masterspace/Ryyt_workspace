4b890073e3726b72de2b3077b3498ec785d40544
shuvams100-code <shuvams100@gmail.com>
2025-12-03 16:49:29 +0530

    Auto-sync: 03-12-2025 16:49:29.38

--- refund-app/src/app/page.tsx
+++ refund-app/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import InteractiveBackground from "@/components/InteractiveBackground";
+import DemoDashboard from "@/components/DemoDashboard";
 
 export default function Home() {
   return (
